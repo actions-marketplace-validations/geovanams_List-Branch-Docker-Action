c27f135f97b4ae789d3a5ac4511ebceaaecf5d88
geovanams <geeovanamartins@outlook.com>
2023-01-19 23:05:03 -0300

    second

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser(description="List Branches")
-parser.add_argument('--owner', help='This is parameter1', required=True)
 parser.add_argument('--repos', help='This is parameter1', required=True)
+parser.add_argument('--owner', help='This is parameter1', required=True)
 
-import requests
-r=requests.get("https://api.github.com/repos/"+parser.parse_args().owner+"/"+parser.parse_args().repos+"/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
 
-# r=requests.get("https://api.github.com/repos/geovanams/hello-world-docker-action/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
+# r=requests.get("https://api.github.com/repos/"+parser.parse_args().owner+"/"+parser.parse_args().repos+"/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
+
+r=requests.get("https://api.github.com/repos/geovanams/hello-world-docker-action/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
 
 print(r.request.path_url)
 
@@ -21,12 +21,3 @@ print(json_formatted_str)
 
 for v in objeto:
     print(v['name'])
-
-
-print(parser.parse_args().owner)
-
-
-
-
-
-
