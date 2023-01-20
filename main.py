6658f1dba93cd3788088e8d4396abee7077aca5f
geovanams <geeovanamartins@outlook.com>
2023-01-19 23:12:57 -0300

    token

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ parser.add_argument('--owner', help='This is parameter1', required=True)
 
 # r=requests.get("https://api.github.com/repos/"+parser.parse_args().owner+"/"+parser.parse_args().repos+"/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
 
-r=requests.get("https://api.github.com/repos/geovanams/hello-world-docker-action/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
+r=requests.get("https://api.github.com/repos/geovanams/hello-world-docker-action/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
 
 print(r.request.path_url)
 
